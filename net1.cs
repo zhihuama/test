4f286b8cdb661cc0ee833cbe2c88b39b504a68cd
mazhihua <649927359@qq.com>
2019-01-04 19:59:46 +0800

    第一次使用创建项目文件
    第一次使用创建项目文件

---
+++ net1.cs
@@ -0,0 +1,23 @@
+using FaceService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ServiceDo.start();
+                Thread.Sleep(1000 * 60 * 2);
+            }
+
+        }
+    }
+}
